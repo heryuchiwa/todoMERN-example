1683ba99ce69574acc8ff8254a169463a84dc7b9
heryuchiwa <herynomenjanahary333@gmail.com>
2022-05-18 11:23:32 +0200

    Mongoose connection

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import cors from "cors";
 
 const app = express();
+const DATABASE_URI = "mongodb+srv://hery:hery@cluster0.2bj8k.mongodb.net/?retryWrites=true&w=majority"
 const PORT = process.env.PORT || 5000 
 
 //MiddleWares
@@ -10,4 +11,7 @@ const PORT = process.env.PORT || 5000
 app.use(cors());
 app.use(express.json())
 
-app.listen(PORT, () => console.log(`server running on ${PORT}`))
+mongoose.connect(DATABASE_URI)
+    .then(app.listen(PORT, () => console.log(`server running on ${PORT}`)))
+    .catch(err => console.log(err))
+
